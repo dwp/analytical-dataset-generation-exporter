8f8708c99fc78c4619d536f95c4b93c67f031232
Gordon Adam <gordon.adam@dwp.gov.uk>
2020-08-10 14:39:16 +0100

    Extracting gauge value from file

--- src/main/java/org/apache/spark/metrics/source/adg/exporter/CollectionProcessingTimeGauge.java
+++ src/main/java/org/apache/spark/metrics/source/adg/exporter/CollectionProcessingTimeGauge.java
@@ -1,12 +1,40 @@
 package org.apache.spark.metrics.source.adg.exporter;
 
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+
 import com.codahale.metrics.Gauge;
 
 public class CollectionProcessingTimeGauge implements Gauge {
 
+    private static String COLLECTION_PROCESSING_TIME_FILE = "/opt/emr/metrics/processing_times.csv"
+    private static String ALL_COLLECTIONS_ID = "all_collections"
+
     @Override
     public Integer getValue() {
-        return 1;
+        try {
+            FileReader fr = new FileReader(COLLECTION_PROCESSING_TIME_FILE)
+            BufferedReader br = new BufferedReader(fr)
+            return extractValue(br);
+        } catch (FileNotFoundException e) {
+            System.out.println(e.toString())
+            return 0;
+        }
     }
-    
+
+    private Integer extractValue(BufferedReader br) {
+        String line;
+        while ((line = br.readLine()) != null) {
+            if line.contains(",") {
+                String[] kvp = line.split(",");
+                if (kvp[0].equals(ALL_COLLECTIONS_ID)) {
+                    return Integer.parseInt(kvp[1]);
+                }
+            }
+        }
+        return 0;
+    }
 }
+
+
