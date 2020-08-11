8005b0f593237a0ba7a89bd2b3e12b5a8b2b4f87
Gordon Adam <gordon.adam@dwp.gov.uk>
2020-08-12 00:25:56 +0100

    Adding support for multiple collections and secrets manager

--- src/main/java/org/apache/spark/metrics/source/adg/exporter/CollectionExporter.java
+++ src/main/java/org/apache/spark/metrics/source/adg/exporter/CollectionExporter.java
@@ -4,10 +4,6 @@ import com.codahale.metrics.Gauge;
 import com.codahale.metrics.MetricRegistry;
 import org.apache.spark.metrics.source.Source;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.Base64;
-
 import com.amazonaws.services.secretsmanager.AWSSecretsManager;
 import com.amazonaws.services.secretsmanager.AWSSecretsManagerClientBuilder;
 import com.amazonaws.services.secretsmanager.model.GetSecretValueRequest;
@@ -18,7 +14,16 @@ import com.amazonaws.services.secretsmanager.model.InvalidParameterException;
 import com.amazonaws.services.secretsmanager.model.DecryptionFailureException;
 import com.amazonaws.services.secretsmanager.model.InternalServiceErrorException;
 
-import org.json.JSONObject;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.Map;
+import java.util.ArrayList;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 
 public class CollectionExporter implements Source {
 
@@ -47,30 +52,30 @@ public class CollectionExporter implements Source {
 
     private ArrayList<String> getMetricList() {
         ArrayList<String> metrics = new ArrayList<String>();
-        JSONObject obj = new JSONObject(getSecret());
-        obj = obj.getJSONObject("collections_all");
-        Iterator<String> keys = obj.keys();
-        while(keys.hasNext()) {
-            String key = keys.next();
-            key = key.replaceAll("db.", "");
-            key = key.replaceAll(".", "_");
-            key = key.replaceAll("-", "_");
+        Gson gson = new Gson();
+        JsonElement jsonElement = new JsonParser().parse(getSecret());
+        JsonObject jsonObject = jsonElement.getAsJsonObject();
+        jsonObject = jsonObject.getAsJsonObject("collections_all");
+        Set<Map.Entry<String, JsonElement>> entries = jsonObject.entrySet();
+        for(Map.Entry<String, JsonElement> entry: entries) {
+            String key = entry.getKey();
+            key = key.replace("db.", "");
+            key = key.replace(".", "_");
+            key = key.replace("-", "_");
             key = key.toLowerCase();
             metrics.add(key);
         }
+        metrics.add("all_collections");
         return metrics;
     }
 
     private String getSecret() {
-
         String secretName = "/concourse/dataworks/adg";
         String region = "eu-west-2";
-
         AWSSecretsManager client  = AWSSecretsManagerClientBuilder.standard().withRegion(region).build();
-        
         GetSecretValueRequest getSecretValueRequest = new GetSecretValueRequest().withSecretId(secretName);
         GetSecretValueResult getSecretValueResult = null;
-
+        String secret;
         try {
             getSecretValueResult = client.getSecretValue(getSecretValueRequest);
         } catch (DecryptionFailureException e) {
@@ -84,14 +89,14 @@ public class CollectionExporter implements Source {
         } catch (ResourceNotFoundException e) {
             throw e;
         }
-
         if (getSecretValueResult.getSecretString() != null) {
             return getSecretValueResult.getSecretString();
         }
         else {
-            String secret = new String(Base64.getDecoder().decode(getSecretValueResult.getSecretBinary()).array());
+            byte[] arry = new byte[getSecretValueResult.getSecretBinary().remaining()];
+            getSecretValueResult.getSecretBinary().get(arry);
+            secret = new String(arry, StandardCharsets.UTF_8);
             return secret;
         }
     }
-    
 }
